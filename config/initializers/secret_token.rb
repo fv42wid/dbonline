bf0a6fcb65806e797198109f415808e3f437c38f
Frank <frank@frank-Studio-1555.(none)>
2014-04-13 12:15:45 -0400

    initial commit, new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbonline::Application.config.secret_token = 'd02d0ba9a1a4881c4ea87954d3ac9a86329f8afc95f09718f4557c95d628affad8193038e778c1beb78f3cce0a016b286980d6f47006de205e21c524ab284fbf'
