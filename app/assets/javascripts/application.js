bfeac474122a70eea7f532e6dedab1f97dc0ba06
Frank <fv42wid@yahoo.com>
2014-04-16 10:09:28 -0400

    column sorting acceptable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,29 @@ $(document).ready(function() {
 
     $('#main_data').tablesorter();
 
-    $('th').click(function() {
-        //if(this.className == '') {
-        $(this).addClass('headerSortDown');
+    //Click Columns to Sort
 
+    $('th').click(function() {
+        //$('th').removeClass();
+        var header = $(this);
+
+        if(header.hasClass('headerSortDown')) {
+            //$(this).removeClass('headerSortDown');
+            $('th').removeClass();
+            $('i').remove();
+            $(this).addClass('headerSortUp');
+            $('<i class="icon-arrow-up"></i>').appendTo(header.find('div:first'));
+
+        } else {
+            //$(this).removeClass('headerSortUp');
+            $('th').removeClass();
+            $('i').remove();
+            $(this).addClass('headerSortDown');
+            $('<i class="icon-arrow-down"></i>').appendTo(header.find('div:first'));
+        }
 
     });
+
+    //Filter rows based on search input fields
 
 });
