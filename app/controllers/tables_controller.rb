1af86dafaccb71a3559d38fe27df46511efe29f4
Frank <fv42wid@yahoo.com>
2014-04-19 17:37:52 -0400

    table loads data from file, now get headers too

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,12 +1,26 @@
 class TablesController < ApplicationController
 
   def index
-    #TODO add error checking for file upload
+
 
   end
 
   def new
-    #TODO parse pipe delimited file into html table
+
+    #TODO add error checking for file upload
+
+  end
+
+  def create
+
+    #TODO get column headers from @header
+    loaded_file = params[:file].tempfile;
+    file_text = File.read(loaded_file);
+
+    @records = file_text.split("\n");
+    @header = @records[0];
+    @records.delete_at(0);
+    #debugger;
 
   end
 
