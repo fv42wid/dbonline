c4156fbb9e7fb636c514b42c9a7956cf79266804
Frank <fv42wid@yahoo.com>
2014-04-19 11:05:37 -0400

    added todos

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,10 +1,12 @@
 class TablesController < ApplicationController
 
   def index
+    #TODO add error checking for file upload
 
   end
 
   def new
+    #TODO parse pipe delimited file into html table
 
   end
 
